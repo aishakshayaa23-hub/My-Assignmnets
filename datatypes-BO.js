e869098f7bfd2ea82eb8ac0061725fba14825a51
aishakshayaa23-hub <aishakshayaa23@gmail.com>
2026-07-11 06:40:55 +0530

    Add files via upload

---
+++ datatypes-BO.js
@@ -0,0 +1,16 @@
+let firstName = "Akshayaa"
+console.log (firstName)
+console.log (typeof firstName)
+let companyName = "Amazon"
+console.log (companyName)
+console.log (typeof companyName)
+let mobileNumber = 8682017915
+console.log (mobileNumber)
+console.log (typeof mobileNumber)
+let isAutomation = true
+console.log (isAutomation)
+console.log (typeof isAutomation)
+let hasPlaywright
+console.log (hasPlaywright)
+console.log(typeof hasPlaywright)
+
