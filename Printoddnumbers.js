2e5af6c823450614744b9939c22d22f0797c94a2
aishakshayaa23-hub <aishakshayaa23@gmail.com>
2026-07-11 07:37:14 +0530

    Add files via upload

---
+++ Printoddnumbers.js
@@ -0,0 +1,17 @@
+function printOddnumbers()
+{
+    for (let i = 0; i <= 25; i++) 
+        {
+        if (i%2!== 0)
+            {
+
+            console.log("no is odd", i);
+
+            }
+            
+        
+    }
+        
+    
+}
+printOddnumbers();
