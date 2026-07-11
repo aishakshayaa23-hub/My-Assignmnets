0e1ebd1338268b76e301c761be197e56ad4b36ff
aishakshayaa23-hub <aishakshayaa23@gmail.com>
2026-07-11 07:07:59 +0530

    Add files via upload

---
+++ Scoping.js
@@ -0,0 +1,18 @@
+let genderType = "female"
+function printGender ()
+{
+ let color = "brown"
+ if (genderType=="female")
+    {
+        var Age = 30
+        let color = "pink"
+        console.log(color);
+              
+
+    }
+ console.log(Age);
+
+   
+}
+printGender ();
+console.log(genderType);
