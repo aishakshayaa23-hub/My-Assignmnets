7ef7caf90fd3206be527f979ab293984500e289f
aishakshayaa23-hub <aishakshayaa23@gmail.com>
2026-07-13 13:32:17 +0530

    Add files via upload

---
+++ numbertype.js
@@ -0,0 +1,21 @@
+//create a function to check nukber type//
+let x = -1
+function numberType()
+{
+   if(x<0)
+   {
+    console.log(x,"is negative");
+    
+   }
+   if(x>0)
+   {
+    console.log(x,"is positive");
+    
+   }
+   else {
+    console.log("Number is zero");
+    
+   }
+
+}
+numberType(x);
